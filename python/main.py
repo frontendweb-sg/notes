2e4707bf30cd00507452245acd7d306f83f22e0d
frontendweb-sg <frontendweb.sg@gmail.com>
2024-08-11 16:21:41 +0530

    update web-scrapping

--- python/main.py
+++ python/main.py
@@ -1,22 +1,6 @@
-
-import concurrent.futures
-
-from threading import Thread, current_thread
-import time
-
-
-def display(n, main):
-    print('n-', current_thread().name)
-    time.sleep(1)
+def counter(n: int):
     for x in range(n):
-        print(f'x-{x}-{main}')
+        yield x
 
 
-start = time.perf_counter()
-t1 = Thread(target=display, kwargs={'n': 100, 'main': 'child-thread'})
-t1.start()
-t1.join()
-end = time.perf_counter()
-
-display(10, 'main-')
-print(f'Finished in {round(end-start, 2)} seconds')
+print(next(counter(5)))
