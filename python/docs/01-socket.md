b14bd0e743741ec5421434712eb0270fc7917fbc
frontendweb-sg <frontendweb.sg@gmail.com>
2024-09-17 12:22:51 +0530

    update pandas

--- python/docs/01-socket.md
+++
@@ -1,3 +0,0 @@
-# What is socket in python?
-
-Socket is a module in python that provide `server-clint` communication using `socket()` method.
--- python/one.py
+++
@@ -1,6 +0,0 @@
-class MyClass:
-    """MyClass created for testing"""
-    name = "PK"
-
-    def say_hi(a):
-        print(f"my name is {MyClass.name}")
