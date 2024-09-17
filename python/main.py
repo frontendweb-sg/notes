07f3b0f92feda6e260defff359366a1febd8613f
frontendweb-sg <frontendweb.sg@gmail.com>
2024-09-17 12:02:34 +0530

    update pandas

--- python/main.py
+++ python/main.py
@@ -1,3 +1,21 @@
-import pandas
+import pandas as pd
+from pydataset import data
+import matplotlib.pyplot as plt
 
-print(pandas.__version__)
+
+l = list(('A', 'B', 'C', 'D', 'E'))
+s = pd.Series(l)
+
+# Data Frame
+
+df1 = pd.DataFrame(s, columns=['A'])
+df2 = pd.DataFrame(s, columns=['B'])
+
+
+df = pd.concat((df1, df2), ignore_index=True)
+df = pd.concat((df1, df2)).sort_index()
+df = pd.concat((df1, df2), keys=['Alpha 1', 'Alpha 2'])
+
+df = pd.concat((df1, df2), axis="columns")
+
+print(df)
