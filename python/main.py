1767835b678ef2e95edbebde74a7e6d1e9417b59
frontendweb-sg <frontendweb.sg@gmail.com>
2024-09-17 17:22:40 +0530

    update python

--- python/main.py
+++ python/main.py
@@ -1,31 +1,6 @@
-import numpy as np
-import pandas as pd
+f = open("csv/employees.csv", "r")
 
-
-food = pd.read_csv("csv/restaurant_foods.csv")
-customers = pd.read_csv("csv/restaurant_customers.csv")
-week_1 = pd.read_csv("csv/restaurant_week_1_sales.csv")
-week_2 = pd.read_csv("csv/restaurant_week_2_sales.csv")
-
-print(week_1.head())
-print(food.head())
-
-df = pd.merge(food, week_1, how="left", on="Food ID")
-
-# print(df)
-
-
-df1 = pd.DataFrame({
-    'key': ['A', 'B', 'C'],
-    'value1': [1, 2, 3]
-})
-
-df2 = pd.DataFrame({
-    'key': ['B', 'C', 'D'],
-    'value2': [4, 5, 6]
-})
-
-# To merge these dataframes on the 'key' column with an inner join:
-result = pd.merge(df1, df2, how='outer', on='key')
-
-print(result)
+for l in f.readline():
+    print(l)
+# line = f.readline()
+f.close()
